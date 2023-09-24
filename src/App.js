b89db6aca6cccb63ded9fb1563f4234d609ed8ba
WinHtunOo29 <winhtunoo12345@gmail.com>
2023-09-24 20:06:13 +0630

    managing state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import Heading from "./Heading";
 import Card from "./Card";
 import Button from "./Button";
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
 import "./App.css";
 
 function App() {
+  const [fruits] = useState([
+    {name: 'apple', id: 1},
+    {name: 'orange', id: 2},
+    {name: 'peach', id: 3}
+  ])
   return (
     <div className="App">
-      <Heading firstName = "Win"/>
+      {/* <Heading firstName = "Win"/>
       <Card h2="First Card's h2" h3="First Card's h3"/>
       <Card h2="Second Card's h2" h3="Second Card's h3"/>
       <Card h2="Third Card's h2" h3="Third Card's h3"/>
-      <Button/>
+      <Button/> */}
+      <Fruits fruits = {fruits}/>
+      <FruitsCounter fruits = {fruits}/>
     </div>
   )
 }
